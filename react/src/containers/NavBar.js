15e93fad719175e3261b3536c59d00ee39f11df8
Marjie Lam <marjielam@gmail.com>
2017-04-27 16:54:02 -0400

    added navbar component

---
+++ react/src/containers/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="navbar">Book Tracker</div>
+    );
+  }
+}
+
+export default NavBar;
--- react/src/main.js
+++ react/src/main.js
@@ -8,17 +8,20 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 import Books from './containers/Books';
+import NavBar from './containers/NavBar';
 
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Books}/>
+      <Route path="/" component={NavBar}>
+        <IndexRoute component={Books}></IndexRoute>
+      </Route>
     </Router>
   </Provider>
 );
 
 if (document.getElementById('current-user-id')) {
-  
+
 }
 
 $(function() {
