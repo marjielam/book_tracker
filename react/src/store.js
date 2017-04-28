0e666d1e7e8d5c104a575599982dd991f1da9d71
Marjie Lam <marjielam@gmail.com>
2017-04-27 22:50:41 -0400

    create users api, fetch current user when navbar mounts

--- react/src/store.js
+++ react/src/store.js
@@ -1,9 +1,10 @@
 /*jshint esversion: 6 */
 
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
 const defaultState = {
   user: null,
@@ -13,7 +14,12 @@ const enhancers = compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
 
-const store = createStore(rootReducer, defaultState, enhancers);
+const store = createStore(
+  rootReducer,
+  defaultState,
+  applyMiddleware(thunk),
+  enhancers
+);
 export const history = syncHistoryWithStore(browserHistory, store);
 
 export default store;
